c54a72d688ac36667051604a3bccb84c47c76739
BotCodeX <78162544+BotCodeX@users.noreply.github.com>
2024-06-10 01:26:02 +0600

    V1.0

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<div>
+  <div class="row font-12px">
+    <div class="col-md-4">
+      <div class="footer">
+        <p class="text-center">
+          <b><a href="#">Terms & Conditions</a></b><br>
+          <a href="#"> Privacy Policy </a><br>
+          <a href="#">Legal</a><br> 
+          <a href="#"> Cara Store</a><br>
+          <a href="#"> FAQ</a><br>
+          <a href="#">Hire</a><br>
+          <a href="#">Press</a><br>
+        </p>
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="footer">
+        <p class="justify">
+        <h4 class="text-center text-red"> Contact us </h4> <br>
+        Address: 141 & 142, Love Road, <br>Tejgaon Industrial Area, <br>Dhaka-1208.<br>
+        Phone: 01216378351<br>
+        Email: support@cara.com<br>
+        </p>
+      </div>
+    </div>
+
+    <div class="col-md-4">       
+      <div class="footer">
+        <h4 class="text-center"><b> Follow Mangola on:</b> </h4>
+        <a href="https://www.facebook.com/anirbandey303"> <img src="images/fb.jpg" class="thumbnail"> </a>
+        <a href="https://www.instagram.com/_anirbandey_"> <img src="images/insta.jpg" class="thumbnail"> </a>
+        <a href="https://www.twitter.com/_anirbandey_"> <img src="images/twitter.jpg" class="thumbnail"> </a>
+      </div>      
+    </div>
+  </div>
+</div>
